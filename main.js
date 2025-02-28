011b68a90afcdec157a50a040ae9b4e6843d1ed4
andre <andrealiuzzo95@gmail.com>
2025-03-01 00:22:34 +0100

    ok

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const ejs = require("ejs")
+const session = require('express-session')
+
 const app = express()
 const port = 3000
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  res.end("<input type='password'>")
-})
+
+const login = require('./routes/login');
+
+// Usa le rotte con un prefisso
+app.use('/login', login);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
