a0a5c4378bf62d3a2bcd0896288b202c49a1e48c
andre <andrealiuzzo95@gmail.com>
2025-03-02 00:55:50 +0100

    no message

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,7 @@
 function auth(req, res, next) {
- 
-
     if (req.session.user) {
         return next(); // Utente autenticato, continua
-        
-        
-    } else {console.log("NA");
+    } else {
         res.redirect("/login"); // Utente non autenticato, reindirizza al login
     }
 }
