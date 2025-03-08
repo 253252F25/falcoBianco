3f9c7c9272d7ddf07734f700baaee8d3303e3f78
Andrea Liuzzo <88933340+253252F25@users.noreply.github.com>
2025-03-08 17:39:44 +0100

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -3,9 +3,9 @@ const con = require('../utils/conn');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    mes = {password_err:""}
+    mes = { password_err: "" }
     if (req.session.password_err) {
-        mes = {password_err:"err"}
+        mes = { password_err: "err" }
     }
     req.session.password_err = false
     res.render("login", mes);
@@ -16,7 +16,7 @@ router.post('/', (req, res) => {
     const { username, password } = req.body;
 
     const query = 'SELECT * FROM utenti WHERE user = ? AND password = ?';
-   
+
     con.query(query, [username, password], (err, result) => {
         if (err) {
             console.error(err);
@@ -26,13 +26,35 @@ router.post('/', (req, res) => {
         if (result.length > 0) {
             // Se trovi l'utente, salva l'utente nella sessione
             req.session.password_err = false;
-            req.session.user = username; 
-        } else { 
+            req.session.id = result[0].id;
+            req.session.user = result[0].user;
+            req.session.admin = result[0].admin;
+        } else {
             // Se l'utente non è trovato o la password è errata
             req.session.password_err = true;
         }
         res.redirect("/index");
-    })  
+    })
+});
+
+router.all('/info', (req, res) => {
+    const { id, user, admin } = req.session;
+    if (req.session.user) {
+        res.json({
+            err: false,
+            data: {
+                id: id,
+                user: user,
+                admin: admin,
+            }
+        });
+    } else {
+        res.json({
+            err: true,
+            data: "No session, Plse log in"
+        });
+    }
+
 });
 
 module.exports = router;
