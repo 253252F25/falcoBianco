1a8602d88f6121ed2c8926652f111bfbd9ce0375
Andrea Liuzzo <88933340+253252F25@users.noreply.github.com>
2025-03-19 10:00:01 +0100

    Update conn.js
    Sostituito conn con pool

--- utils/conn.js
+++ utils/conn.js
@@ -1,11 +1,14 @@
 var mysql = require('mysql2');
 
-var con = mysql.createConnection({
-    host: process.env.DB_HOST, // Usa le variabili d'ambiente per la connessione
+var con = mysql.createPool({
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port:process.env.DB_PORT
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 con.on('end', () => {
