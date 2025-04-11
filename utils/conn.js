999d0d1e4d8ed94efff84c1dcbe8441d10338235
Andrea Liuzzo <88933340+253252F25@users.noreply.github.com>
2025-04-11 13:18:16 +0200

    Update conn.js

--- utils/conn.js
+++ utils/conn.js
@@ -1,21 +1,25 @@
 const { MongoClient } = require('mongodb');
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-let db;
+let cachedClient = null;
+let cachedDb = null;
 
 async function connectToDatabase() {
-  if (!db) {
-    await client.connect();
-    db = client.db(process.env.DB_NAME);
-    console.log('Connesso a MongoDB Atlas');
+  if (cachedDb) return cachedDb;
+
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+  if (!cachedClient) {
+    cachedClient = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    await cachedClient.connect();
   }
-  return db;
+
+  cachedDb = cachedClient.db(process.env.DB_NAME);
+  console.log('✅ Connesso a MongoDB Atlas (cached)');
+  return cachedDb;
 }
 
 module.exports = connectToDatabase;
