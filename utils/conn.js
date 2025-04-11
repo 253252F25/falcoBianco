165c474ff3a75aec306662f3a838b676fcdccf7f
Andrea Liuzzo <88933340+253252F25@users.noreply.github.com>
2025-04-11 14:12:57 +0200

    Update conn.js

--- utils/conn.js
+++ utils/conn.js
@@ -1,25 +1,27 @@
-const { MongoClient } = require('mongodb');
-
-let cachedClient = null;
-let cachedDb = null;
-
 async function connectToDatabase() {
-  if (cachedDb) return cachedDb;
+  try {
+    console.log("🔄 Iniziando la connessione a MongoDB...");
+    if (cachedDb) {
+      console.log("⚡️ MongoDB già connesso (cachedDb)");
+      return cachedDb;
+    }
 
-  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+    console.log(uri)
+    if (!cachedClient) {
+      cachedClient = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("🌐 Connettendo a MongoDB...");
+      await cachedClient.connect();
+      console.log("✅ Connesso a MongoDB Atlas (nuova connessione)");
+    }
 
-  if (!cachedClient) {
-    cachedClient = new MongoClient(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    await cachedClient.connect();
+    cachedDb = cachedClient.db(process.env.DB_NAME);
+    return cachedDb;
+  } catch (err) {
+    console.error("❌ Errore durante la connessione a MongoDB:", err.message);
+    throw err;
   }
-
-  cachedDb = cachedClient.db(process.env.DB_NAME);
-  console.log('✅ Connesso a MongoDB Atlas (cached)');
-  return cachedDb;
 }
-
-module.exports = connectToDatabase;
